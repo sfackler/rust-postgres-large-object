d14b279d6f50b71afd87b5591a53d40833f2281f
Steven Fackler <sfackler@gmail.com>
2015-04-05 00:35:59 +0000

    rebuild pages at c09addf8ac36e0878733bc95f1bc41aaf30bbf5b

--- doc/implementors/std/io/trait.Seek.js
+++ doc/implementors/std/io/trait.Seek.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['postgres_large_object'] = ["<a class='stability Unstable' title='Unstable: the central `seek` method may be split into multiple methods instead of taking an enum as an argument'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Seek.html' title='std::io::Seek'>Seek</a> for <a class='struct' href='postgres_large_object/struct.LargeObject.html' title='postgres_large_object::LargeObject'>LargeObject</a>&lt;'a&gt;",];
+implementors['postgres_large_object'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Seek.html' title='std::io::Seek'>Seek</a> for <a class='struct' href='postgres_large_object/struct.LargeObject.html' title='postgres_large_object::LargeObject'>LargeObject</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
