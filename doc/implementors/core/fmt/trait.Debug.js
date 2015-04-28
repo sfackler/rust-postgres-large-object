a109bb1fa85bd770ca9abbb95a89973df884463e
Steven Fackler <sfackler@gmail.com>
2015-04-28 06:28:47 +0000

    rebuild pages at 4cf83a00c6bc71ca4d9c58ff399498398fe8b090

--- doc/implementors/core/fmt/trait.Debug.js
+++ doc/implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['postgres_large_object'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='postgres_large_object/enum.Mode.html' title='postgres_large_object::Mode'>Mode</a>","<a class='stability Stable' title='Stable'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='postgres_large_object/struct.LargeObject.html' title='postgres_large_object::LargeObject'>LargeObject</a>&lt;'a&gt;",];
+implementors['postgres_large_object'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='postgres_large_object/enum.Mode.html' title='postgres_large_object::Mode'>Mode</a>","impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='postgres_large_object/struct.LargeObject.html' title='postgres_large_object::LargeObject'>LargeObject</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/core/ops/trait.Drop.js
+++ doc/implementors/core/ops/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['postgres_large_object'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='postgres_large_object/struct.LargeObject.html' title='postgres_large_object::LargeObject'>LargeObject</a>&lt;'a&gt;",];
+implementors['postgres_large_object'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='postgres_large_object/struct.LargeObject.html' title='postgres_large_object::LargeObject'>LargeObject</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
