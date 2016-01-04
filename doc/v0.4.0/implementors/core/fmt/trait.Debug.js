475b8ca551c877065cd9cb4e2c86b9ac17cc2bce
Steven Fackler <sfackler@gmail.com>
2016-01-03 16:09:23 -0800

    Release v0.4.0

---
+++ doc/v0.4.0/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres_large_object'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='postgres_large_object/enum.Mode.html' title='postgres_large_object::Mode'>Mode</a>","impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='postgres_large_object/struct.LargeObject.html' title='postgres_large_object::LargeObject'>LargeObject</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
